dfa5f30881c2aa971eb25b03ca18c0a64ea8691c
SwissKazTravel <eyesofargos@gmail.com>
2025-10-28 18:55:09 +0100

    Create page.tsx

---
+++ app/[locale]/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: 'center', marginTop: '50px' }}>
+      <img src="/logo.png" alt="SwissKaz Travel Logo" width="250" />
+      <h1>Welcome to SwissKaz Travel</h1>
+      <p>DISCOVER THE UNDISCOVERED</p>
+    </main>
+  );
+}
