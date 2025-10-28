06fac2e0171776431489d38c607e39299eab7996
SwissKazTravel <eyesofargos@gmail.com>
2025-10-28 18:55:37 +0100

    Delete page.tsx

--- page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ textAlign: 'center', marginTop: '50px' }}>
-      <img src="/logo.png" alt="SwissKaz Travel Logo" width="250" />
-      <h1>Welcome to SwissKaz Travel</h1>
-      <p>DISCOVER THE UNDISCOVERED</p>
-    </main>
-  );
-}
